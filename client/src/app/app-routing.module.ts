8b6ecc822cb07ffd12012001acc4487d674b272b
ikakko <ikakocore@gmail.com>
2023-08-22 17:21:38 +0400

    client side routing and lazy loading added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: HomeComponent},
+  // lazy loading, shop module and it's registered routes
+  {path: 'shop',loadChildren: () =>
+   import('./shop/shop.module')
+   .then(mod => mod.ShopModule)
+  },
+  {path: '**',redirectTo: '',pathMatch:'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IBrand } from '../shared/models/brand';
 import { IType } from '../shared/models/productType';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 @Injectable({
   providedIn: 'root'
 })
@@ -52,6 +53,10 @@ export class ShopService {
       })
     )
     
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(`${this.baseUrl}products/${id}`);
   }
 
   getBrands() {
